a6e0f192f7f37f05f37703784e7c73fd79281dc2
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-08-19 11:18:49 +0530

    password encrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const path = require("path");
+const bcrypt=require('bcrypt');
+const { error } = require("console");
 
 exports.getSignupForm = (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "signup.html"));
@@ -9,18 +11,20 @@ exports.saveUser = async (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-
   try {
     const existingUser = await User.findOne({ where: { email: email } });
     if (existingUser) {
       return res.send("This email address is already registered.");
     }
+  const saltSource=10;
+  bcrypt.hash(password,saltSource,async(err,hash)=>{
     const newUser = await User.create({
       name: name,
       email: email,
-      password: password,
+      password: hash,
     });
     return res.redirect("/user/login");
+  }) 
   } catch (err) {
     console.log(err);
     return res.status(500).send("Internal Server Error");
@@ -41,23 +45,24 @@ exports.getLoginPage = (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "login.html"));
 };
 
-exports.login = async (req, res, next) => {
-    
-const email = req.body.email;
-  const password = req.body.password;
-  console.log(email);
-  console.log("entered password " + password);
+exports.login = async (req, res, next) => { 
+  const email = req.body.email;
+  const password = req.body.password; 
   try {
     const user = await User.findOne({ where: { email: email } });
-    console.log("user password " + user.password);
     if (!user) {
       return res.json({ exists: false, message: "User not found" });
-    }
-    if (user.password === password) {
-      return res.json({ exists: true, data: user });
-    } else {
-      return res.status(401).json({ exists: false, message: "Incorrect password" });
-    }
+    } 
+    bcrypt.compare(password,user.password,(err,result)=>{
+      if(err){
+        throw new error('Something went wrong')
+      }
+      if (result === true) {
+        return res.json({ exists: true, message: 'loggin Successfully' });
+      } else {
+        return res.status(401).json({ exists: false, message: "Incorrect password" });
+      }
+    })
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal Server Error" });
